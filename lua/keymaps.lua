e47fadba20a21fae1599916d357af58f7e8ceddc
Augusto Lee <alee@ibiunainvest.com.br>
2025-11-19 19:00:21 -0300

    some keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,6 +7,8 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- TODO Refactor this to somewhere else
 vim.keymap.set('n', '<leader>e', '<cmd>Yazi', { desc = '[E]xplore Yazi' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
