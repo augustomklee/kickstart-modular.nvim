d63fa861831a6084a6453a10d44a7386c97446fc
Augusto Lee <alee@ibiunainvest.com.br>
2025-11-19 13:59:02 -0300

    Added yazi plugin

---
+++ lua/custom/plugins/yazi.lua
@@ -0,0 +1,55 @@
+return {
+  ---@type LazySpec
+  {
+    'mikavilpas/yazi.nvim',
+    version = '*', -- use the latest stable version
+    event = 'VeryLazy',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim', lazy = true },
+    },
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      --
+      {
+        -- Yazi Options
+        '<leader>e',
+        mode = { 'n' },
+        '<cmd>Yazi',
+        desc = '[E]xplore Yazi',
+      },
+      {
+        '<leader>ee',
+        mode = { 'n', 'v' },
+        '<cmd>Yazi<cr>',
+        desc = 'Open yazi at the current file',
+      },
+      {
+        -- Open in the current working directory
+        '<leader>ec',
+        '<cmd>Yazi cwd<cr>',
+        desc = "Open the file manager in nvim's working directory",
+      },
+      {
+        '<leader>er',
+        '<cmd>Yazi toggle<cr>',
+        desc = 'Resume the last yazi session',
+      },
+    },
+    ---@type YaziConfig | {}
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+      keymaps = {
+        show_help = '<f1>',
+        copy_relative_path_to_selected_files = nil,
+      },
+    },
+    -- 👇 if you use `open_for_directories=true`, this is recommended
+    init = function()
+      -- mark netrw as loaded so it's not loaded at all.
+      --
+      -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+      vim.g.loaded_netrwPlugin = 1
+    end,
+  },
+}
